435b44251c00be7bda8ed095f07aefe333b7a82d
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-05-22 01:39:35 +0000

    Built site for gh-pages

--- dados/prep-agua-rede1.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-agua_rede1 <- read_csv2("https://raw.githubusercontent.com/luisfelipebr/mti/master/dados/agua_rede1.csv", locale = readr::locale(encoding="ISO-8859-1")
-                   )
-
-agua_rede1 |> 
-  janitor::clean_names() |> 
-  write_csv2("praticas/dados/agua_rede1.csv")
--- praticas/dados/idhm.R
+++
@@ -1,5 +0,0 @@
-library(abjData)
-dados_filtrados <- abjData::pnud_min |> 
-  dplyr::filter(ano == max(ano), uf_sigla == "SP") 
-
-readr::write_csv(dados_filtrados, "praticas/dados/idhm_sp_2010.csv")  
